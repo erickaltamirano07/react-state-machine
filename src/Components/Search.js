2fb1a6db34bd1b6260f9d1e463dac67a7cc5e206
Erick Altamirano <erickaltamirano07@hotmail.com>
2025-04-10 22:14:08 -0500

    Jerarquia y Paralelismo

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,44 +1,27 @@
-import React, { useState } from "react";
-import "./Search.css";
+import React, { useState } from 'react';
+import './Search.css';
 
 export const Search = ({ send }) => {
-  const [flight, setFlight] = useState("");
+  const [flight, setFlight] = useState('');
 
   const goToPassengers = () => {
-    send("CONTINUE", { selectedCountry: flight });
-  };
+    send('CONTINUE', { selectedCountry: flight })
+  }
 
   const handleSelectChange = (event) => {
     setFlight(event.target.value);
   };
 
-  const options = ["Mexico", "Venezuela", "Colombia"];
+  const options = ['Mexico', 'Venezuela', 'Colombia'];
 
   return (
-    <div className="Search">
-      <p className="Search-title title">Busca tu destino</p>
-      <select
-        id="country"
-        className="Search-select"
-        value={flight}
-        onChange={handleSelectChange}
-      >
-        <option value="" disabled defaultValue>
-          Escoge un país
-        </option>
-        {options.map((option) => (
-          <option value={option} key={option}>
-            {option}
-          </option>
-        ))}
+    <div className='Search'>
+      <p className='Search-title title'>Busca tu destino</p>
+      <select id="country" className='Search-select' value={flight} onChange={handleSelectChange}>
+        <option value="" disabled defaultValue>Escoge un país</option>
+        {options.map((option) => <option value={option} key={option}>{option}</option>)}
       </select>
-      <button
-        onClick={goToPassengers}
-        disabled={flight === ""}
-        className="Search-continue button"
-      >
-        Continuar
-      </button>
+      <button onClick={goToPassengers} disabled={flight === ''} className='Search-continue button'>Continuar</button>
     </div>
   );
-};
+}; 
